0c49d2e82bcbfcc15ac4e979c68e9afd257373bc
Amit Schnitzer <amit.schnitzer@gmail.com>
2026-04-06 15:03:46 -0400

    adding an S3 bucket to the party

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,3 +9,37 @@ resource "aws_vpc" "main" {
     Name = "Drift-Demo-BOT"
   }
 }
+
+resource "aws_s3_bucket" "drift_demo" {
+  bucket = "aschnitzer-drift-detection-bot"
+
+  tags = {
+    Name = "Drift-Demo-BOT"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "drift_demo" {
+  bucket = aws_s3_bucket.drift_demo.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "drift_demo" {
+  bucket = aws_s3_bucket.drift_demo.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "drift_demo" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.drift_demo,
+    aws_s3_bucket_public_access_block.drift_demo,
+  ]
+
+  bucket = aws_s3_bucket.drift_demo.id
+  acl    = "public-read"
+}
